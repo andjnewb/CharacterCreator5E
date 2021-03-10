c8355cffabd554793f6d805b565667e52184e640
andjnewb <fidolimp@gmail.com>
2021-03-09 23:40:41 -0600

    jhkb

--- 5ECharacterCreator.cs
+++ 5ECharacterCreator.cs
@@ -12,10 +12,30 @@ namespace CharacterCreator5E
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateNewCharacter_Click(object sender, EventArgs e)
+        {
+            //In Form1, we want to open the NewCharacter form when the create a character button is clicked
+            var newCharWindow = new NewCharacter();
+            
+
+
+            newCharWindow.Show();
+        }
     }
 }
---
+++ NewCharacter.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace CharacterCreator5E
+{
+    partial class NewCharacter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.AvatarFileButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.NameEntry = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Choose Your Avatar and Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(578, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            this.openFileDialog1.Title = "Select an avatar";
+            // 
+            // AvatarFileButton
+            // 
+            this.AvatarFileButton.Location = new System.Drawing.Point(578, 164);
+            this.AvatarFileButton.Name = "AvatarFileButton";
+            this.AvatarFileButton.Size = new System.Drawing.Size(197, 29);
+            this.AvatarFileButton.TabIndex = 2;
+            this.AvatarFileButton.Text = "Open File";
+            this.AvatarFileButton.UseVisualStyleBackColor = true;
+            this.AvatarFileButton.Click += new System.EventHandler(this.AvatarFileButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 27);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Name:";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NameEntry
+            // 
+            this.NameEntry.AcceptsReturn = true;
+            this.NameEntry.Location = new System.Drawing.Point(71, 57);
+            this.NameEntry.Name = "NameEntry";
+            this.NameEntry.Size = new System.Drawing.Size(162, 27);
+            this.NameEntry.TabIndex = 4;
+            this.NameEntry.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = this.textBox1.Controls;
+            // 
+            // NewCharacter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NameEntry);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.AvatarFileButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "NewCharacter";
+            this.Text = "NewCharacter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button AvatarFileButton;
+        private System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox NameEntry;
+        public System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
