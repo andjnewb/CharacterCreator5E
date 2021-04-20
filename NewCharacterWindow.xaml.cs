e893440f44e96ca3cd49bb799330f8bac8b8e947
ANDREW NEWBILL <fidolimp@gmail.com>
2021-04-19 20:35:30 -0500

    More progress on racial bonuses
    Added Dwarf, Elf, Dragonborn, and Gnome's racial bonuses. The next commit will finish up with the rest of the races, including those that are user selectable.

--- NewCharacterWindow.xaml.cs
+++ NewCharacterWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System;
 
 namespace GroupProject5ECharCreator
 {
@@ -14,6 +15,7 @@ namespace GroupProject5ECharCreator
         //Holds data related to the 5EDatabase.accdb file
         DataContainer container = new DataContainer();
         CharDataContainer CharContainer = new CharDataContainer();
+        RaceFactory factory;
         public NewCharacterWindow()
         {
             InitializeComponent();
@@ -21,6 +23,25 @@ namespace GroupProject5ECharCreator
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //Hide the racial bonus selection stuff, as only certain Races can select their bonuses.
+            Bonus_1_Combo_Box.Visibility = Visibility.Hidden;
+            Bonus_2_Combo_Box.Visibility = Visibility.Hidden;
+            Racial_Bonus_Selection_Label.Visibility = Visibility.Hidden;
+
+            //This sucks but its filling the Bonus boxes up with all of the stats
+            Bonus_1_Combo_Box.Items.Add("Strength");
+            Bonus_2_Combo_Box.Items.Add("Strength");
+            Bonus_1_Combo_Box.Items.Add("Dexterity");
+            Bonus_2_Combo_Box.Items.Add("Dexterity");
+            Bonus_1_Combo_Box.Items.Add("Charisma");
+            Bonus_2_Combo_Box.Items.Add("Charisma");
+            Bonus_1_Combo_Box.Items.Add("Intelligence");
+            Bonus_2_Combo_Box.Items.Add("Intelligence");
+            Bonus_1_Combo_Box.Items.Add("Wisdom");
+            Bonus_2_Combo_Box.Items.Add("Wisdom");
+            Bonus_1_Combo_Box.Items.Add("Constitution");
+            Bonus_2_Combo_Box.Items.Add("Constitution");
+
             //Load up the RaceListBox with the races in DataContainer
             foreach (string str in container.races)
             {
@@ -79,6 +100,7 @@ namespace GroupProject5ECharCreator
 
         private void SaveContainer()
         {
+            //Serialize the current CharDataContainer, and store it in a .sav file
             IFormatter formatter = new BinaryFormatter();
             Stream stream = new FileStream(CharContainer.CharName + ".sav", FileMode.Create, FileAccess.Write, FileShare.None);
             formatter.Serialize(stream, CharContainer);
@@ -92,41 +114,106 @@ namespace GroupProject5ECharCreator
 
         private void RaceListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Constitution_Bonus_Box.Content = "";
-            StrengthBonusBox.Content = "";
-            Dexterity_Bonus_Box.Content = "";
-            RaceFactory factory;
+       
             
 
-            //This switch catches whatever race the user has selected, and assigns factory with correct type of factory. It then uses that factory to assign CharContainer a race.
-            switch (RaceListBox.SelectedItem.ToString())
-            {
-                case "Dwarf":
-                    CharContainer.RaceName = "Dwarf";
-                    //Using the factory method to set the race for our dwarf. Same will apply bellow
-                    factory = new DwarfFactory();
-                    CharContainer.race = factory.GetRace();
-                    
-                    
-                    
-                    break;
-                case "Elf":
-                    CharContainer.RaceName = "Elf";
-                    factory = new ElfFactory();
-                    CharContainer.race = factory.GetRace();
-                    break;
-                default:
-                    break;
-            }
-
-
-
+            //Cleaning up the bonus boxes if the race is changed.
             foreach (Label label in BonusGrid1.Children)
             {
                 label.Content = null;
             }
 
 
+            //This switch catches whatever race the user has selected, and assigns factory with correct type of factory. It then uses that factory to assign CharContainer a race.
+            switch (RaceListBox.SelectedItem.ToString())
+            {
+                case "Dwarf":
+                    Bonus_1_Combo_Box.Visibility = Visibility.Hidden;
+                    Bonus_2_Combo_Box.Visibility = Visibility.Hidden;
+
+                    CharContainer.RaceName = "Dwarf";
+                    //Using the factory method to set the race for our dwarf. Same will apply below
+                    factory = new DwarfFactory();
+                    CharContainer.race = factory.GetRace();
+                    ModifyBonusBoxes();
+                    break;
+                case "Elf":
+                    Bonus_1_Combo_Box.Visibility = Visibility.Hidden;
+                    Bonus_2_Combo_Box.Visibility = Visibility.Hidden;
+                    CharContainer.RaceName = "Elf";
+                    factory = new ElfFactory();
+                    CharContainer.race = factory.GetRace();
+                    ModifyBonusBoxes();
+                    break;
+                case "Half Elf":
+                    Bonus_1_Combo_Box.Visibility = Visibility.Visible;
+                    Bonus_2_Combo_Box.Visibility = Visibility.Visible;
+
+                    
+
+                    //return new HalfElfFactory();
+
+                    break;
+                case "Dragonborn":
+                    Bonus_1_Combo_Box.Visibility = Visibility.Hidden;
+                    Bonus_2_Combo_Box.Visibility = Visibility.Hidden;
+                    CharContainer.RaceName = "Dragonborn";
+                    factory = new DragonBornFactory();
+                    CharContainer.race = factory.GetRace();
+                    ModifyBonusBoxes();
+                    break;
+                case "Gnome":
+                    Bonus_1_Combo_Box.Visibility = Visibility.Hidden;
+                    Bonus_2_Combo_Box.Visibility = Visibility.Hidden;
+                    CharContainer.RaceName = "Gnome";
+                    factory = new GnomeFactory();
+                    CharContainer.race = factory.GetRace();
+                    ModifyBonusBoxes();
+                    break;
+                default:
+                    break;
+            }
+
+
+            
+
+        }
+
+        void ModifyBonusBoxes()
+        {
+
+            //Here,
+            foreach (var bonus in CharContainer.race.bonuses)
+            {
+                switch (bonus.Item1)
+                {
+                    case RacialBonus.ConstBonus:
+                        Constitution_Bonus_Box.Content = "+" + bonus.Item2.ToString();
+                        break;
+                    case RacialBonus.StrBonus:
+                        Strength_Bonus_Box.Content = "+" + bonus.Item2.ToString();
+                        break;
+                    case RacialBonus.CharismaBonus:
+                        Charisma_Bonus_Box.Content = "+" + bonus.Item2.ToString();
+                        break;
+                    case RacialBonus.DexBonus:
+                        Dexterity_Bonus_Box.Content = "+" + bonus.Item2.ToString();
+                        break;
+                    case RacialBonus.IntBonus:
+                        Intelligence_Bonus_Box.Content = "+" + bonus.Item2.ToString();
+                        break;
+                    case RacialBonus.WisdomBonus:
+                        Wisdom_Bonus_Box.Content = "+" + bonus.Item2.ToString();
+                            break;
+                    default:break;
+                }
+
+
+            }
         }
     }
+
+    
+
+
 }
--- RaceFactory.cs
+++ RaceFactory.cs
@@ -23,13 +23,9 @@ namespace GroupProject5ECharCreator
     [Serializable]
     public abstract class Race
     {
-        
+        public List<(RacialBonus, int)> bonuses;//Contains all of the bonuses for that race. It is a list, since some races have multiple or let you select which bonuses you want. 
 
-
-
-        public virtual void GetRace()
-        {
-        }
+        public string RaceName;
     }
 
     //RACES//
@@ -37,21 +33,14 @@ namespace GroupProject5ECharCreator
     public class Dwarf : Race
     {
         //This class inherits from the abstract class Race
-        int bonusAmt;
-        public Dwarf(int bonusAmount)
+        int _ConstBonusAmt;
+        public Dwarf(int ConstBonusAmount)
         {
-            bonusAmt = bonusAmount;
-        }
+            _ConstBonusAmt = ConstBonusAmount;
 
-        public override RacialBonus bonus
-        {
-            get { return _bonus; }
-            set {; }
-        }
+            bonuses =  new List<(RacialBonus, int)> { (RacialBonus.ConstBonus, _ConstBonusAmt)};//This is our list of tuples. Dwarves, for examples. get a constitution bonus of +2. For some reason, I can't get .Add() to work for this list so we're going with initializing it here for now.
+            RaceName = "Dwarf"; //Might not be necessary, but could be useful for later.
 
-        public override void GetRace()
-        {
-            
         }
 
     }
@@ -60,61 +49,35 @@ namespace GroupProject5ECharCreator
     public class Elf : Race
     {
         //This class inherits from the abstract class Race
-        private RacialBonus _bonus;
-        int bonusAmt;
-        public Elf(int bonusAmount)
+        int _DexterityBonusAmt;
+        public Elf(int DexterityBonusAmount)
         {
-            _bonus = RacialBonus.DexBonus;
-            bonusAmt = bonusAmount;
+            
+            _DexterityBonusAmt = DexterityBonusAmount;
+            bonuses = new List<(RacialBonus, int)> { (RacialBonus.DexBonus, _DexterityBonusAmt) };
+            RaceName = "Elf";
         }
-
-        public override RacialBonus bonus
-        {
-            get { return _bonus; }
-            set {; }
-        }
-
-        public override void GetRace()
-        {
-
-        }
-
     }
 
     [Serializable]
     public class Dragonborn : Race 
     {
         //Dragonborn get two racial bonuses. This is a work around.
-        private RacialBonus _bonus1;
-        private RacialBonus _bonus2;
-        private int _bonusAmount1;
-        private int _bonusAmount2;
 
-        public Dragonborn(int bonusAmount1, int bonusAmount2)
+        int _StrBonusAmt;
+        int _CharismaBonusAmt;
+        public Dragonborn(int StrBonusAmount, int CharismaBonusAmount)
         {
-            _bonus1 = RacialBonus.StrBonus;
-            _bonus2 = RacialBonus.CharismaBonus;
-            _bonusAmount1 = bonusAmount1;
-            _bonusAmount2 = bonusAmount2;
+            _StrBonusAmt = StrBonusAmount;
+            _CharismaBonusAmt = CharismaBonusAmount;
+
+
+            bonuses = new List<(RacialBonus, int)> { (RacialBonus.StrBonus, _StrBonusAmt) , (RacialBonus.CharismaBonus, _CharismaBonusAmt) };
+            RaceName = "Dragonborn";
 
         }
 
-        public override RacialBonus bonus
-        {
-            get { return _bonus1; }
-            set {; }
-        }
-
-        public  RacialBonus bonus2
-        {
-            get { return _bonus2; }
-            set {; }
-        }
-
-        public override void GetRace()
-        {
-           
-        }
+   
 
     }
 
@@ -122,74 +85,28 @@ namespace GroupProject5ECharCreator
     public class Gnome : Race
     {
         //This class inherits from the abstract class Race
-        private RacialBonus _bonus;
-        int bonusAmt;
-        public Gnome(int bonusAmount)
+        int _IntBonusAmt;
+        public Gnome(int IntBonusAmount)
         {
-            _bonus = RacialBonus.IntBonus;
-            bonusAmt = bonusAmount;
-        }
+       
+            _IntBonusAmt = IntBonusAmount;
 
-        public override RacialBonus bonus
-        {
-            get { return _bonus; }
-            set {; }
-        }
-
-        public override void GetRace()
-        {
-
+            bonuses = new List<(RacialBonus, int)> { (RacialBonus.IntBonus, _IntBonusAmt) };
+            RaceName = "Gnome";
         }
     }
 
     [Serializable]
     public class HalfElf : Race 
     {
-        //This class inherits from the abstract class Race
+        //The situation is different for a half-elf, since they get to pick 2 bonuses to add +1 to, in addition to their inherent +2 to charisma. Half-elf OP
 
-        //Half-elves get a base +2 to charisma, plus they get +1 to two other abilities of their choosing. This is why we have a different constructor.
-        private RacialBonus _bonus;//This is the inherit bonus
-        private RacialBonus _bonusChoice1;
-        private RacialBonus _bonusChoice2;
-        private int _bonusAmount;//Inherit bonus amount.
-        private int _bonusChoiceAmount1;
-        private int _bonusChoiceAmount2;
-        public HalfElf(RacialBonus choice1, RacialBonus choice2, int bonusAmount, int bonusChoiceAmount1, int bonusChoiceAmount2)
+
+        public HalfElf(RacialBonus choice1, RacialBonus choice2)
         {
-            _bonus = RacialBonus.ConstBonus;
-            _bonusChoice1 = choice1;
-            _bonusChoice2 = choice2;
-
-            _bonusAmount = bonusAmount;
-
-            _bonusChoiceAmount1 = bonusChoiceAmount1;
-            _bonusChoiceAmount2 = bonusChoiceAmount2;
-
+            bonuses = new List<(RacialBonus, int)> { (RacialBonus.CharismaBonus, 2) ,(choice1, 1), (choice2,1)};
         }
 
-        public override RacialBonus bonus
-        {
-            //returns inherit bonus
-            get { return _bonus; }
-            set {; }
-        }
-
-        public RacialBonus bonusChoice1
-        {
-            get { return _bonusChoice1; }
-            set {; }
-        }
-
-        public RacialBonus bonusChoice2
-        {
-            get { return _bonusChoice2; }
-            set {; }
-        }
-
-        public override void GetRace()
-        {
-
-        }
     }
 
 
@@ -231,27 +148,47 @@ namespace GroupProject5ECharCreator
     public class DragonBornFactory : RaceFactory
     {
 
-        int _strengthBonus;
-        int _charismaBonus;
 
-        public DragonBornFactory(int strengthBonus, int charismaBonus)
+
+        public DragonBornFactory()
         {
-            _strengthBonus = strengthBonus;
-            _charismaBonus = charismaBonus;
         }
 
         public override Race GetRace()
         {
             //2 strength, 1 charisma
-            return new Dragonborn(_charismaBonus, _strengthBonus);
+            return new Dragonborn(2, 1);
         }
 
     }
 
-    //public class HalfElfFactory : RaceFactory
-    //{
-    //    public HalfElfFactory()
-    //}
+    public class GnomeFactory : RaceFactory 
+    {
+        public GnomeFactory()
+        { }
+
+        public override Race GetRace()
+        {
+            return new Gnome(2);
+        }
+    }
+
+    public class HalfElfFactory : RaceFactory
+    {
+        RacialBonus _choice1;
+        RacialBonus _choice2;
+
+        public HalfElfFactory(RacialBonus choice1, RacialBonus choice2)
+        {
+            _choice1 = choice1;
+            _choice2 = choice2;
+        }
+
+        public override Race GetRace()
+        {
+            return new HalfElf(_choice1, _choice2);
+        }
+    }
 
 
 
